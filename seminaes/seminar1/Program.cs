082b864c176f5e9abf026d410468d140400c0475
AlexFilOrel <orel-alex@ya.ru>
2022-11-19 20:53:46 +0300

    Ready HW 1

---
+++ seminaes/seminar1/Program.cs
@@ -0,0 +1,60 @@
+﻿/*Напишите программу, которая на вход принимает числj и вычисляет его квадрат.*/
+
+/*Console.WriteLine("Input number ");
+int num = Convert.ToInt32(Console.ReadLine());
+int quad = num * num;
+//Console.WriteLine("Quad of " + num + "->" + quad);
+Console.WriteLine($"Quad of {num} -> {quad}");*/
+
+
+
+/*Напишите программу, которая на вход принимает два числа и проверяет, является ли первое число квадратом второго.
+a = 25; b = 5 -> да
+a = 2 b = 10 -> нет
+a = 9; b = -3 -> да
+a = -3 b = 9 -> нет*/
+
+/*Console.WriteLine("Input number 1 ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input number 2 ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+int quad = num2 * num2;
+
+if(quad == num1)
+{
+    Console.WriteLine($"{num1} is quad of {num2}");
+}
+else
+{
+    Console.WriteLine($"{num1} is not quad of {num2}");
+}*/
+
+
+
+/*Напишите программу, которая на вход принимает одно число (N), а на выходе показывает все целые числа в промежутке от -N до N.
+4 -> "-4, -3, -2, -1, 0, 1, 2, 3, 4"
+2 -> " -2, -1, 0, 1, 2"*/
+
+/*Console.WriteLine("Input number ");
+int num = Convert.ToInt32(Console.ReadLine());
+int start = num * (-1);
+while (start <= num)
+{
+ Console.Write(start + " ");
+ start = start + 1;
+}*/
+
+
+
+/*Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает **последнюю** цифру этого числа.
+456 -> 6
+782 -> 2
+918 -> 8*/
+
+/*Console.WriteLine("Input number ");
+int num = Convert.ToInt32(Console.ReadLine());
+num = num % 10;
+Console.WriteLine(num);*/
+
+
