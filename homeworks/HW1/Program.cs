8b8de2e1b9b20da8c0b63df6841da515a5a59567
AlexFilOrel <orel-alex@ya.ru>
2022-11-23 20:06:20 +0300

    Change HW 1

--- homeworks/HW1/Program.cs
+++ homeworks/HW1/Program.cs
@@ -34,7 +34,7 @@ if (num1 > max) max = num1;
 if (num2 > max) max = num2;
 if (num3 > max) max = num3;
 
-Console.WriteLine($"The maximim number is {max}");*/
+Console.WriteLine($"The maximum number of {num1}, {num2}, {num3} is {max}.");*/
 
 
 
