963b988715e1b9dde716e47e744e989c1f4a0009
AlexFilOrel <orel-alex@ya.ru>
2022-11-26 14:54:32 +0300

    Ready HW4

---
+++ homeworks/HW4/Program.cs
@@ -0,0 +1,70 @@
+﻿//Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+//3, 5 -> 243 (3⁵)
+//2, 4 -> 16
+
+/*int Exponentiation(int num1, int num2)
+{
+    int exp = num1;
+    for (int i = 1; i < num2; i++)
+    {
+        exp = exp * num1;
+    }
+    return exp;
+}
+Console.WriteLine("Input number A: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input number B: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+int result = Exponentiation(number1, number2);
+Console.WriteLine($"The number {number1} increased to the exponent of {number2} is a number {result}.");*/
+
+
+
+//Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+//452 -> 11
+//82 -> 10
+//9012 -> 12
+
+/*int SumDigitNumber(int num) 
+{
+    int sum = 0;
+    for(; num > 0; num = num / 10) 
+    {
+        sum = sum + num % 10;
+    }
+    return sum;
+}
+Console.WriteLine("Input number : ");
+int number = Convert.ToInt32(Console.ReadLine());
+int result = SumDigitNumber(number);
+Console.WriteLine($"The sum of the digits of {number} is {result}.");*/
+
+
+
+//Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+//1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+//6, 1, 33 -> [6, 1, 33]
+
+/*int[] CreateRandArray(int size)
+{
+    int[]newArray = new int [size];
+    for(int i = 0; i < size; i++)
+    {
+        Console.WriteLine($"Input {i+1} number ");
+        newArray[i] = Convert.ToInt32(Console.ReadLine());
+    }
+    return newArray;
+}
+void PrintArray(int[]array)
+{
+    Console.Write("The received array -> ");
+    for(int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
+Console.WriteLine("Input size ");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] myArray = CreateRandArray(size);
+PrintArray(myArray);*/
---
+++ seminaes/seminar4/Program.cs
@@ -0,0 +1,137 @@
+﻿//Задача 1. Напишите программу, которая принимает на вход число (А) и выдаёт сумму чисел от 1 до А.
+//7 -> 28
+//4 -> 10
+//8 -> 36
+
+/*int FindSum (int num)
+{
+    int sum = 0;
+    for(int i = 1; i <= num; i++)
+    {
+        sum = sum + i;
+    }
+    return sum;
+}
+Console.Write("Input number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int result = FindSum(number);
+Console.WriteLine($"The sum of the numbers from 1 to {number} is {result}.");*/
+
+
+
+//Задача 2.Напишите программу, которая принимает на вход число и выдаёт количество цифр в числе.
+//456 -> 3
+//78 -> 2
+//89126 -> 5
+
+/*int FindQuantityDigits(int num)
+{
+    int count = 0;
+    
+    while(num > 0)
+    {
+      num = num / 10;
+      count++;
+    }
+    for (count = 0; a > 0; count++){
+        a = a / 10;
+    }
+    return count;
+}
+Console.Write("Input number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int result = FindQuantityDigits(number);
+Console.WriteLine($"Quantity of digits in a number {number} is {result}.");*/
+
+//-----------------------------
+
+/*int FindQuantityDigits(int num)
+{
+    int count = 0;
+    for (count = 0; num > 0; count++)
+    {
+        num = num / 10;
+    }
+    return count;
+}
+Console.Write("Input number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int result = FindQuantityDigits(number);
+Console.WriteLine($"Quantity of digits in a number {number} is {result}.");*/
+
+
+
+//Задача 3. Напишите программу, которая принимает на вход число N и выдаёт произведение чисел от 1 до N.
+//4  -> 24
+//5  -> 120
+
+/*int Factorial(int num)
+{
+    if(num == 1) return 1;
+    else return num * Factorial(num - 1);
+}
+Console.Write("Input number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int result = Factorial(number);
+Console.WriteLine($"The product of numbers from 1 to {number} is {result}.");*/
+
+//----------------------------------------------
+
+/*int Factorial(int num)
+{
+    int result = 1;
+    for(int i = 1; i <= num; i++)
+    {
+        result = result * i;
+    }
+    return result;
+}
+Console.Write("Input number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int result = Factorial(number);
+Console.WriteLine($"The product of numbers from 1 to {number} is {result}.");*/
+
+
+
+//Задача 4. Напишите программу, которая выводит массив из 8 элементов, заполненный нулями и единицами в случайном порядке.
+// [1,0,1,1,0,1,0,0]
+
+/*void FillArray (int size)
+{
+    int[] array = new int [size];
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = new Random().Next(0,2);
+        Console.Write($"{array[i]} ");
+    }
+}
+FillArray (8);*/
+
+//--------------------------------------
+
+/*int[] CreateRandArray(int size, int minValue, int maxValue)
+{
+    int[]newArray = new int [size];
+    for(int i = 0; i < size; i++)
+    {
+        newArray[i] = new Random().Next(minValue,maxValue + 1);
+    }
+    return newArray;
+}
+void PrintArray(int[]array)
+{
+    Console.Write("The resulting array -> ");
+    for(int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
+Console.WriteLine("Введите размер массива");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите минимальное значение элемента");
+int min = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите максимальное значение элемента");
+int max = Convert.ToInt32(Console.ReadLine());
+int[] myArray = CreateRandArray(a, min, max);
+PrintArray(myArray);*/
