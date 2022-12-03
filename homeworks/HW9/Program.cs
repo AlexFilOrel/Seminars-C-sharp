88cc245612c7d4ecf581afd5d26374491a4e9f74
AlexFilOrel <orel-alex@ya.ru>
2022-12-03 21:42:01 +0300

    Ready HW 9

---
+++ homeworks/HW9/Program.cs
@@ -0,0 +1,77 @@
+﻿//Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+//N = 5 -> "5, 4, 3, 2, 1"
+//N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+/*void ShowNumbers (int n)
+{
+    if(n >= 1)
+    {
+                Console.Write (n + " ");
+        ShowNumbers(n - 1);
+    }
+}
+Console.Write("Input number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+Console.Write ($"N = {num} -> ");
+ShowNumbers(num);
+Console.WriteLine ();*/
+
+
+
+//Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+//M = 1; N = 15 -> 120
+//M = 4; N = 8. -> 30
+
+/*int FindSumNumbers (int m, int n)
+{
+    if(m != n)
+    {
+        if(m < n)
+        {
+            return FindSumNumbers(m + 1, n) + m;
+        }
+        else
+        {
+            return FindSumNumbers(m,n + 1) + n;
+        }
+    }
+    else return m;
+}
+Console.Write("Input number M: ");
+int numM = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input number N: ");
+int numN = Convert.ToInt32(Console.ReadLine());
+Console.Write ($"M = {numM}; N = {numN}. -> ");
+int sum = FindSumNumbers(numM, numN);
+Console.WriteLine (sum);*/
+
+
+
+//Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+//m = 2, n = 3 -> A(m,n) = 9
+//m = 3, n = 2 -> A(m,n) = 29
+
+/*int FunctionAkkerman(int m, int n)
+{
+    if (m == 0)
+    {
+        return n + 1;
+    }
+    else if (n == 0 && m > 0)
+    {
+        return FunctionAkkerman(m - 1, 1);
+    }
+    else
+    {
+        return (FunctionAkkerman(m - 1, FunctionAkkerman(m, n - 1)));
+    }
+}
+
+
+Console.Write("Input number M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input number N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write($"m = {m}, n = {n} -> A({m},{n}) = ");
+Console.Write(FunctionAkkerman(m,n));
+Console.WriteLine();*/
---
+++ seminaes/seminar9/Program.cs
@@ -0,0 +1,90 @@
+﻿//Задача 1. Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от 1 до N.
+//N = 5 -> "1, 2, 3, 4, 5"
+//N = 6 -> "1, 2, 3, 4, 5, 6"
+
+/*void ShowNumbers (int n)
+{
+    if(n >= 1)
+    {
+        ShowNumbers(n - 1);//рекурсия
+        Console.Write (n + " ");
+    }
+}
+Console.WriteLine("Input number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+ShowNumbers(num);
+Console.WriteLine ();*/
+
+
+
+//Задание 2. Напишите программу, которая будет принимать на вход число и возвращать сумму его цифр.
+//453 -> 12
+//45 -> 9
+
+/*int FindSum (int num)
+{
+    if(num != 0)
+    {
+        return FindSum(num / 10) + num % 10;
+    }
+    else
+    return num;
+}
+
+Console.WriteLine("Input number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int sum = FindSum(num);
+Console.WriteLine (sum);
+Console.WriteLine ();*/
+
+
+
+//Задача 3. Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
+//M = 1; N = 5. -> "1, 2, 3, 4, 5"
+//M = 4; N = 8. -> "4, 5, 6, 7, 8"
+
+/*void ShowNumbers (int m, int n)
+{
+    if(m != n)
+    {
+        if(n > m)
+        {
+            ShowNumbers(m,n - 1);//рекурсия
+            Console.Write (n + " ");
+        }
+        else
+        {
+            ShowNumbers(m - 1,n);//рекурсия
+            Console.Write (m + " ");
+        }
+    }
+    else Console.Write (m + " ");
+}
+Console.WriteLine("Input number m: ");
+int numM = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input number n: ");
+int numN = Convert.ToInt32(Console.ReadLine());
+ShowNumbers(numM,numN);
+Console.WriteLine ();*/
+
+
+//Задача 4.Напишите программу, которая на вход принимает два числа A и B, и возводит число А в целую степень B с помощью рекурсии.
+//A = 3; B = 5 -> 243 (3⁵)
+//A = 2; B = 3 -> 8
+
+/*int FindExtent (int a, int b)
+{
+    if (b == 0) return 1;
+    else
+    {
+        return FindExtent(a,b - 1) * a;
+    }
+}
+
+Console.WriteLine("Input number A: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input number B: ");
+int b = Convert.ToInt32(Console.ReadLine());
+int extent = FindExtent(a, b);
+Console.WriteLine (extent);
+Console.WriteLine ();*/
